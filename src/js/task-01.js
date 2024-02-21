df654e1b8830f8354aa0c4ddfbf0d8148e2c1324
MagadouP <152959026+MagadouP@users.noreply.github.com>
2024-02-21 14:37:03 +0100

    Update task-01.js
    second-change

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,8 @@
 const categories = document.querySelectorAll("#categories .item");
 
+const numberOfCategories = categories("Number of categories:", categories.length);
 
 categories.forEach(category => {
   const title = category.querySelector("h2").textContent;
   const elements = category.querySelectorAll("ul li").length;
-  console.log("Category:", title);
-  console.log("Elements:", elements);
 });
