da1919a0fe126ddcd68cb9df7964562d6c1f626b
MagadouP <152959026+MagadouP@users.noreply.github.com>
2024-02-21 15:03:46 +0100

    Update task-01.js
    get me to origin

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,7 +1,5 @@
 const categories = document.querySelectorAll("#categories .item");
 
-
-
 categories.forEach(category => {
   const title = category.querySelector("h2").textContent;
   const elements = category.querySelectorAll("ul li").length;
