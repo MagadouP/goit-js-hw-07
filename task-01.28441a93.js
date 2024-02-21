6f1f66e91ce6d40f540d6ab3b5a0684dace6c07c
MagadouP <152959026+MagadouP@users.noreply.github.com>
2024-02-21 12:41:09 +0000

    Deploying to gh-pages from @ MagadouP/goit-js-hw-07@c2f996a294367a54e4fac086b97e146f84113282 🚀

---
+++ task-01.28441a93.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("#categories .item").forEach((e=>{const l=e.querySelector("h2").textContent,o=e.querySelectorAll("ul li").length;console.log("Category:",l),console.log("Elements:",o)}));
+//# sourceMappingURL=task-01.28441a93.js.map
---
+++ task-02.4f28d584.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("ul#ingredients"),t=["Potatoes","Mushrooms","Garlic","Tomatos","Herbs","Condiments"].map((e=>{const t=document.createElement("li");return t.textContent=e,t.classList.add("item"),t}));e.append(...t);
+//# sourceMappingURL=task-02.4f28d584.js.map
